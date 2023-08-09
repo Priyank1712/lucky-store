a4c1269e88806a6dce6250794ff2ff6daf8c2144
Priyank1712 <pharjilawala07@gmail.com>
2023-08-09 12:41:47 -0700

    feat: single product page UI completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
+import Product from './pages/Product';
 
 function App() {
   return (
-    <Home />
+    <Product />
   );
 }
 
